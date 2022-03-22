f1ba4d540a64d5357f68e886347ad68ec0bd96c9
margaretau <aumarg.edu@gmail.com>
2022-03-21 17:24:08 -0700

    Updated vtt, link article, custom controls, overall content, timing, functionality

--- js/main.js
+++ js/main.js
@@ -7,15 +7,20 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     var myCues = [
         { seconds: 2, callback: func1 },
-        { seconds: 14, callback: func10 },
-        { seconds: 39, callback: func2 },
-        { seconds: 42, callback: func3 },
+        { seconds: 12, callback: func10 },
+        { seconds: 41, callback: func2 },
+        { seconds: 44, callback: func3 },
+        { seconds: 57, callback: func40 },
         { seconds: 61, callback: func4 },
         { seconds: 78, callback: func5 },
+        { seconds: 99, callback: func50 },
         { seconds: 110, callback: func6 },
+        { seconds: 125, callback: func60 },
+        { seconds: 134, callback: func61 },
         { seconds: 156, callback: func7 },
-        { seconds: 163, callback: func8 },
-        { seconds: 172, callback: func9 },
+        { seconds: 163, callback: func70 },
+        { seconds: 168, callback: func8 },
+        { seconds: 174, callback: func9 },
     ];
 
     cueTimer.setup("vid", myCues);
@@ -53,7 +58,14 @@ function func3() {
     document.querySelector("#web").src =
         "https://en.wikipedia.org/wiki/Essential_amino_acid";
 }
-
+function func40() {
+    let pop = document.querySelector(".pop");
+    pop.innerHTML = "<p>Complete Protein Foods: <br> Provide all 9 essential amino acids.</p>";
+    document.querySelector(".pop").classList.toggle("hide");
+    setTimeout(() => {
+        document.querySelector(".pop").classList.toggle("hide");
+    }, 2000);
+}
 function func4() {
     document.querySelector("#web").src =
         "../assets/cleveland_clinic.html";
@@ -63,8 +75,27 @@ function func5() {
     document.querySelector("#web").src =
         "https://www.webmd.com/food-recipes/protein";
 }
+function func50() {
+    let pop = document.querySelector(".pop");
+    pop.innerHTML = "<p>3500 calories = 1 pound!</p>";
+    document.querySelector(".pop").classList.toggle("hide");
+    setTimeout(() => {
+        document.querySelector(".pop").classList.toggle("hide");
+    }, 2000);
+}
 
 function func6() {
+    document.querySelector("#web").src = "https://www.medicalnewstoday.com/articles/protein-intake";
+}
+function func60() {
+    let pop = document.querySelector(".pop");
+    pop.innerHTML = "<p>Caution: <br>Excessive protein can be detrimental.</p>";
+    document.querySelector(".pop").classList.toggle("hide");
+    setTimeout(() => {
+        document.querySelector(".pop").classList.toggle("hide");
+    }, 2000);
+}
+function func61() {
     document.querySelector("#web").src = "../assets/harvard_health.html";
 }
 
@@ -79,7 +110,7 @@ function func7() {
 } 
 function func70() {
     const pop = document.querySelector(".pop");
-    pop.innerHTML = "<p>Well, serious athletes need more protein.</p>";
+    pop.innerHTML = "<p>Well, ATHLETES may need more protein.</p>";
     pop.classList.toggle("hide");
     setTimeout(() => {
         document.querySelector(".pop").classList.toggle("hide");
@@ -89,7 +120,7 @@ function func70() {
 
 function func8() {
     const pop = document.querySelector(".pop");
-    pop.innerHTML = "<p>Don't overdo it. Calculate your protein needs.</p>";
+    pop.innerHTML = "<p>Don't overdo it! <br> Calculate your protein needs.</p>";
     pop.classList.toggle("hide");
     setTimeout(() => {
         document.querySelector(".pop").classList.toggle("hide");
@@ -99,7 +130,7 @@ function func8() {
 }
 function func9() {
     const pop = document.querySelector(".pop");
-    pop.innerHTML = "<p>Note - Evidence is still not conclusive!</p>";
+    pop.innerHTML = "<p>Note - Clinical data is still not conclusive!</p>";
     pop.classList.toggle("hide");
     setTimeout(() => {
         document.querySelector(".pop").classList.toggle("hide");
